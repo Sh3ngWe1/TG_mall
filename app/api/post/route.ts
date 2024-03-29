08d822ef24cf20fa22ae149a1c393ba5d82c4651
Tommy <tommylin12025@gmail.com>
2024-03-29 17:48:02 +0800

    [add]api/POST insert an data into test table

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,30 +1,25 @@
-// pages/api/yourRoute.js
-
 import { createClient } from "@/utils/supabase/server";
-
 export default async function handler(req: any, res: any) {
   if (req.method === "POST") {
     const supabase = createClient();
 
-    // 新增一筆資料到 "test" 資料表中
-    const { data, error } = await supabase
-      .from("test")
-      .insert([{ id: 2, time: "24:00:00", name: "John" }]);
+    // 刪除ID為1的資料
+    const { data, error } = await supabase.from("test").delete().eq("id", 1);
 
     if (error) {
       // 如果出現錯誤，回傳錯誤訊息
-      res
-        .status(500)
-        .json({
-          code: 500,
-          msg: "Error adding data to the database",
-          data: null,
-        });
+      res.status(500).json({
+        code: 500,
+        msg: "Error deleting data from the database",
+        data: null,
+      });
     } else {
-      // 如果成功新增資料，回傳成功訊息和新增的資料
-      res
-        .status(200)
-        .json({ code: 200, msg: "Data added successfully", data: data });
+      // 如果成功刪除資料，回傳成功訊息和刪除的資料
+      res.status(200).json({
+        code: 200,
+        msg: "Data deleted successfully",
+        data: data,
+      });
     }
   } else {
     // 如果收到的是非POST請求，回傳405錯誤
