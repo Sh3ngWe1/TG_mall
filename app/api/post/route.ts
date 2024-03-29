7e668332fe11b2e3ffce348e5b669e7d977d8020
Tommy <tommylin12025@gmail.com>
2024-03-29 17:40:28 +0800

    [add]api/POST insert an data into test table

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,36 +1,33 @@
+// pages/api/yourRoute.js
+
 import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 
-// export async function POST() {
-//     const supabase = createClient();
-//     const {data:t} = await supabase.from("test").select();
-//     if (!t) {
-//         return Response.json({code: 401, msg: "",data: ""});
-//     }
-//     return Response.json({code: 200, msg: "", data: {data:t}});
-// }
+export default async function handler(req: any, res: any) {
+  if (req.method === "POST") {
+    const supabase = createClient();
 
-export async function POST() {
-  const supabase = createClient();
+    // 新增一筆資料到 "test" 資料表中
+    const { data, error } = await supabase
+      .from("test")
+      .insert([{ id: 2, time: "24:00:00", name: "John" }]);
 
-  // 新增一筆資料到 "test" 資料表中
-  const { data, error } = await supabase
-    .from("test")
-    .insert([{ id: 2, name: "John" }]);
-
-  if (error) {
-    // 如果出現錯誤，回傳錯誤訊息
-    return Response.json({
-      code: 500,
-      msg: "Error adding data to the database",
-      data: null,
-    });
+    if (error) {
+      // 如果出現錯誤，回傳錯誤訊息
+      res
+        .status(500)
+        .json({
+          code: 500,
+          msg: "Error adding data to the database",
+          data: null,
+        });
+    } else {
+      // 如果成功新增資料，回傳成功訊息和新增的資料
+      res
+        .status(200)
+        .json({ code: 200, msg: "Data added successfully", data: data });
+    }
   } else {
-    // 如果成功新增資料，回傳成功訊息和新增的資料
-    return Response.json({
-      code: 200,
-      msg: "Data added successfully",
-      data: data,
-    });
+    // 如果收到的是非POST請求，回傳405錯誤
+    res.status(405).end();
   }
 }
