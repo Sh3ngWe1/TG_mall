3f43d86799121e987a603fc5739d45faa37fd370
Tommy <tommylin12025@gmail.com>
2024-04-01 18:06:32 +0800

    [update]post/route.ts && test2

--- app/api/POST/route.ts
+++ app/api/POST/route.ts
@@ -3,29 +3,29 @@ import { redirect } from "next/navigation";
 import test from "node:test";
 import { textSpanContainsPosition } from "typescript";
 
-// export async function GET() {
-//   const supabase = createClient();
-//   const { data: t } = await supabase.from("test").select();
-//   if (!t) {
-//     return Response.json({ code: 401, msg: "", data: "" });
-//   }
-//   return Response.json({ code: 200, msg: "", data: { data: t } });
-// }
-
-export async function POST() {
+export async function GET() {
   const supabase = createClient();
-  const { data: t, error } = await supabase.from("test").insert({
-    id: "2",
-    created_at: "",
-    name: "John",
-  });
-  if (error) {
-    throw error;
-  }
+  const { data: t } = await supabase.from("test").select();
   if (!t) {
     return Response.json({ code: 401, msg: "", data: "" });
   }
   return Response.json({ code: 200, msg: "", data: { data: t } });
-  // console.log('Inserted data:', t);
-  //return Response.json({ code: 200, msg: "", data: { data: t } });
 }
+
+// export async function POST() {
+//   const supabase = createClient();
+//   const { data: t, error } = await supabase.from("test").insert({
+//     id: "2",
+//     created_at: "",
+//     name: "John",
+//   });
+//   if (error) {
+//     throw error;
+//   }
+//   if (!t) {
+//     return Response.json({ code: 401, msg: "", data: "" });
+//   }
+//   return Response.json({ code: 200, msg: "", data: { data: t } });
+//   // console.log('Inserted data:', t);
+//   //return Response.json({ code: 200, msg: "", data: { data: t } });
+// }
--- app/api/test2/route.ts
+++ app/api/test2/route.ts
@@ -1,34 +1,24 @@
-// import { createClient } from "@/utils/supabase/server";
-// import { redirect } from "next/navigation";
-
-// export async function PUT() {
-//   const supabase = createClient();
-
-//   const { data: t, error } = await supabase
-//     .from("test")
-//     .delete()
-//     .match({ id: 1 });
-
-//   if (!t) {
-//     return Response.json({ code: 401, msg: "", data: "" });
-//   }
-//   return Response.json({ code: 200, msg: "", data: { data: t } });
-// }
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
-export async function GET() {
-  // const res = await fetch('https://data.mongodb-api.com/...', {
-  //     headers: {
-  //         'Content-Type': 'application/json',
-  //         'API-Key': process.env.DATA_API_KEY,
-  //     },
-  // })
-  // const data = await res.json()
+export async function DELETE() {
   const supabase = createClient();
-  const { data: t } = await supabase.from("test").select();
+
+  const { data: t, error } = await supabase.from("test").delete().eq("id", 1);
+
   if (!t) {
     return Response.json({ code: 401, msg: "", data: "" });
   }
   return Response.json({ code: 200, msg: "", data: { data: t } });
 }
+// import { createClient } from "@/utils/supabase/server";
+// import { redirect } from "next/navigation";
+
+// export async function GET() {
+//   const supabase = createClient();
+//   const { data: t } = await supabase.from("test").select();
+//   if (!t) {
+//     return Response.json({ code: 401, msg: "", data: "" });
+//   }
+//   return Response.json({ code: 200, msg: "", data: { data: t } });
+// }
