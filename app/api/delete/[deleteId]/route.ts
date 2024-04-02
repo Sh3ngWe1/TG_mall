0b18af451cd191528a30ae3dd209f16b2367d250
Tommy <tommylin12025@gmail.com>
2024-04-03 01:58:04 +0800

    [Update]api/delete/[deleteId]/route.ts=>'invalid input syntax for type bigint', [ADD]test2: available for update data.

--- app/api/delete/[deleteId]/route.ts
+++ app/api/delete/[deleteId]/route.ts
@@ -1,12 +1,31 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import { NextRequest } from "next/server";
 
-export async function PUT() {
+// export async function DELETE(
+//   req: NextRequest,
+//   { params: { deleteId } }: { params: { deleteId: string } }
+// ) {
+//   // const data = await res.json()
+//   const supabase = createClient();
+//   const { data: t } = await supabase
+//     .from("test")
+//     .select()
+//     .eq("id", deleteId)
+//     .single();
+
+//   if (!t) {
+//     return Response.json({ code: 401, msg: "", data: "" });
+//   }
+//   return Response.json({ code: 200, msg: "", data: { data: t } });
+// }
+export async function GET(deleteId: Number) {
   const supabase = createClient();
   const { data, error } = await supabase
     .from("test")
-    .update({ name: "John" })
-    .eq("id", 1);
+    .delete()
+    .eq("id", deleteId)
+    .single();
 
   if (error) {
     return Response.json({ code: 401, msg: error.message, data: "" });
