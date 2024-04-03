d3f778e51f2bb27a3ef1acd00d40975c6c035ad4
Tommy <tommylin12025@gmail.com>
2024-04-03 15:34:14 +0800

    [UPDATE]update/route.ts: id/name && insert/id/name

--- app/api/insert/[id]/[name]/route.ts
+++ app/api/insert/[id]/[name]/route.ts
@@ -4,13 +4,15 @@ import { NextRequest } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params: { id } }: { params: { id: string } }
+  { params: { id, name } }: { params: { id: string; name: string } }
 ) {
   const supabase = createClient();
-  const { data, error } = await supabase.from("test").insert([{ id: id }]);
+  const { data, error } = await supabase
+    .from("test")
+    .insert([{ id: id, name: name, status: 1 }]);
 
   if (error) {
     return Response.json({ code: 401, msg: error.message, data: "" });
   }
-  return Response.json({ code: 200, msg: "", data: { data } });
+  return Response.json({ code: 200, msg: "成功新增", data: { data } });
 }
--- app/api/update/[id]/[name]/route.ts
+++ app/api/update/[id]/[name]/route.ts
@@ -1,15 +1,19 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import { NextRequest } from "next/server";
 
-export async function GET() {
+export async function GET(
+  req: NextRequest,
+  { params: { id, name } }: { params: { id: string; name: string } }
+) {
   const supabase = createClient();
   const { data, error } = await supabase
     .from("test")
-    .update({ name: "AAABBBCCC" })
-    .eq("id", 1);
+    .update({ name: name })
+    .eq("id", id);
 
   if (error) {
     return Response.json({ code: 401, msg: error.message, data: "" });
   }
-  return Response.json({ code: 200, msg: "", data: { data } });
+  return Response.json({ code: 200, msg: "成功更新", data: { data } });
 }
